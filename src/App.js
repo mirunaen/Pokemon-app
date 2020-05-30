886452745c3437d5c6b61b3790c8abb1c04b87b3
mirunaen <mirunaen@gmail.com>
2020-05-30 19:40:29 +0200

    use axios to fetch the pokeapi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import PokemonList from "./PokemonList";
+import axios from "axios";
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
+  const [pokemon, setPokemon] = useState();
+  axios.get("https://pokeapi.co/api/v2/pokemon");
   return <PokemonList pokemon={pokemon} />;
 }
 
