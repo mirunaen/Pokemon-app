4c3474acb35176bf37553e276a6278f4f66e3ce3
mirunaen <mirunaen@gmail.com>
2020-05-30 19:43:41 +0200

    setPokemon to the name of the pokemons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import PokemonList from "./PokemonList";
 import axios from "axios";
 function App() {
   const [pokemon, setPokemon] = useState();
-  axios.get("https://pokeapi.co/api/v2/pokemon");
+  axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+    setPokemon(res.data.results.map((p) => p.name));
+  });
   return <PokemonList pokemon={pokemon} />;
 }
 
