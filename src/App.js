a426742104b090e591e8d8c341f5200124d7eab8
mirunaen <mirunaen@gmail.com>
2020-05-30 19:46:49 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
 import axios from "axios";
 function App() {
-  const [pokemon, setPokemon] = useState();
-  axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
-    setPokemon(res.data.results.map((p) => p.name));
-  });
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      setPokemon(res.data.results.map((p) => p.name));
+    });
+  }, []);
+
   return <PokemonList pokemon={pokemon} />;
 }
 
