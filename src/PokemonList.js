470145fe0d90cde478fd2fe4c01267b9987aa1da
mirunaen <mirunaen@gmail.com>
2020-05-31 15:41:46 +0200

    some css from bootstrap to make it pretty

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function PokemonList({ pokemon }) {
   return (
-    <div>
+    <div className="col-md-3 offset-md-2">
       {pokemon.map((p) => (
         <div key={p}>{p} </div>
       ))}
